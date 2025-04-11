41478fef00aba64df49848e18bfa51613fa8361a
AerieL114 <nguyentuanlongqng@gmail.com>
2025-04-11 14:44:00 +0700

    GUI update: LoaiTietKiem is now dropdown & added checks for deposits

--- GUI/runapp.py
+++ GUI/runapp.py
@@ -3,6 +3,7 @@ import os
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 from LoginGUI import LoginGUI
+
 if __name__ == "__main__" : 
     app = LoginGUI()
     app.run()
